0de88236916587519788bff3cca247af12877ad3
daniel.morais <daniel.morais@pipefy.com>
2025-09-23 08:40:06 -0300

    inprove example

--- examples/resources/scaffolding_example/resource.tf
+++ examples/resources/scaffolding_example/resource.tf
@@ -46,6 +46,45 @@ resource "pipefy_field" "title" {
   required = true
 }
 
+
+resource "pipefy_field" "translation" {
+  phase_id = pipefy_phase.backlog.id
+  type     = "short_text"
+  label    = "Translation"
+  required = true
+}
+
+resource "pipefy_automation" "example_ai" {
+  name           = "Example AI Generation"
+  event_id       = "card_created"
+  action_id      = "generate_with_ai"
+  event_repo_id  = pipefy_pipe.test.id
+  action_repo_id = pipefy_pipe.test.id
+  active         = true
+
+  # action_params and condition must be JSON strings
+  action_params = jsonencode({
+    aiParams = {
+      value    = "Translate to german: %%{${pipefy_field.title.internal_id}}"
+      fieldIds = [pipefy_field.translation.internal_id]
+    }
+  })
+
+  # no filtering
+  condition = jsonencode({
+    expressions = [{
+      structure_id  = 0
+      field_address = ""
+      operation     = ""
+      value         = ""
+    }]
+    expressions_structure = [[0]]
+  })
+}
+
+
 output "pipe_id" { value = pipefy_pipe.test.id }
 output "phase_id" { value = pipefy_phase.backlog.id }
-output "field_id" { value = pipefy_field.title.id }
+output "field_id" { value = pipefy_field.title.internal_id }
+output "translation_field_id" { value = pipefy_field.translation.internal_id }
+output "automation_id" { value = pipefy_automation.example_ai.id }
