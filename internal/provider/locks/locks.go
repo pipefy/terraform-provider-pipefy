4d7a88ab637b1b39209ccd39c29ebb62bd7853f1
daniel.morais <daniel.morais@pipefy.com>
2025-10-11 23:29:59 -0300

    appease golangci

--- internal/provider/locks/locks.go
+++ internal/provider/locks/locks.go
@@ -1,6 +1,3 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 package locks
 
 import "sync"
@@ -9,7 +6,10 @@ var repoLocks sync.Map
 
 func LockRepo(repoID string) func() {
 	muI, _ := repoLocks.LoadOrStore(repoID, &sync.Mutex{})
-	mu := muI.(*sync.Mutex)
+	mu, ok := muI.(*sync.Mutex)
+	if !ok {
+		panic("expected *sync.Mutex only in repoLocks")
+	}
 	mu.Lock()
 	return mu.Unlock
 }
